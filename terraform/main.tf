5d6f4403d10f86167398f0bd73d1cf143a7a8e41
Oleg Bilovus <68842946+olegbilovus@users.noreply.github.com>
2024-10-18 18:05:44 +0200

    set-up basics

---
+++ terraform/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    azurerm = {
+        source  = "hashicorp/azurerm"
+        version = "4.6.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  subscription_id = var.sub_id
+  features {
+  }
+}
+
+resource "random_string" "random_suffix" {
+  length  = 5
+  special = false
+  upper   = false
+  keepers = {
+    seed = 16754
+  }
+}
+
+locals {
+  rnd_str = random_string.random_suffix.result
+}
+
+resource "azurerm_resource_group" "this" {
+  name     = var.name
+  location = var.location
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,13 @@
+variable "sub_id" {
+  type = string
+  sensitive = true
+}
+
+variable "name" {
+  default = "emad_restapi"
+}
+
+variable "location" {
+  default = "francecentral"
+}
+
