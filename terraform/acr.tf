a20abe616bda0bffed5c846009cdf51f2c248543
Oleg Bilovus <68842946+olegbilovus@users.noreply.github.com>
2024-10-18 19:13:23 +0200

    azure container registry

---
+++ terraform/acr.tf
@@ -0,0 +1,28 @@
+resource "azurerm_container_registry" "this" {
+  name                = "${var.name}${local.rnd_str}cr"
+  resource_group_name = azurerm_resource_group.this.name
+  location            = var.location
+  sku                 = "Standard"
+  admin_enabled       = true
+}
+
+# ACR Task to build and push the image to ACR
+resource "azurerm_container_registry_task" "build-image" {
+  name                  = "${var.name}buildimage"
+  container_registry_id = azurerm_container_registry.this.id
+  platform {
+    os           = "Linux"
+    architecture = "amd64"
+  }
+  docker_step {
+    dockerfile_path      = "Dockerfile"
+    context_path         = var.gh_repo
+    context_access_token = var.gh_access_token
+    image_names          = ["${var.name}:latest"]
+  }
+}
+
+# Run the task, it will run only once even after doing another appy
+resource "azurerm_container_registry_task_schedule_run_now" "build-image" {
+  container_registry_task_id = azurerm_container_registry_task.build-image.id
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,13 +1,23 @@
 variable "sub_id" {
-  type = string
+  type      = string
   sensitive = true
 }
 
 variable "name" {
-  default = "emad_restapi"
+  default = "emadrestapi"
 }
 
 variable "location" {
   default = "francecentral"
+}
+
+variable "gh_repo" {
+  type      = string
+  sensitive = true
+}
+
+variable "gh_access_token" {
+  type      = string
+  sensitive = true
 }
 
