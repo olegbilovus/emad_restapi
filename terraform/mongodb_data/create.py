a28b3da212d7cb8d3ddd4197382ace49c1b26795
Oleg Bilovus <68842946+olegbilovus@users.noreply.github.com>
2024-10-30 17:18:06 +0100

    populate the mongodb with all the available json files. Removed unused fields

--- terraform/mongodb_data/create.py
+++ terraform/mongodb_data/create.py
@@ -5,9 +5,11 @@ import pymongo
 
 MONGODB_URI = os.environ.get('MONGODB_URI')
 
-FILE_PATH = os.environ.get('FILE_PATH')
+JSONS_DIR = os.environ.get('JSONS_DIR')
 DATABASE_NAME = 'pictograms'
 COLLECTION_NAME = 'pictograms'
+
+FILES_PATH = [os.path.join(JSONS_DIR, f) for f in os.listdir(JSONS_DIR) if f.endswith('.json')]
 
 db = pymongo.MongoClient(MONGODB_URI)[DATABASE_NAME]
 
@@ -16,12 +18,41 @@ if db[COLLECTION_NAME].count_documents({}) > 0:
     print('Data already exists in the database')
     skip_data_insertion = True
 
+pictograms = {}
+langs = []
 if not skip_data_insertion:
-    with open(FILE_PATH, 'r') as f:
-        jsonData = json.load(f)
-        db[COLLECTION_NAME].insert_many(jsonData)
+    for i, file in enumerate(FILES_PATH):
+        with open(file, 'r', encoding="utf8") as f:
+            jsonData = json.load(f)
+            lang = f.name.split(os.sep)[-1].split('.')[0]
+            langs.append(lang)
+            for data in jsonData:
+                if i == 0:
+                    pictograms[data['_id']] = {
+                        '_id': data['_id'],
+                        'sex': data['sex'],
+                        'violence': data['violence'],
+                        'keywords': {
+                            lang: data['keywords']
+                        }
+                    }
+                else:
+                    try:
+                        pictograms[data['_id']]['keywords'][lang] = data['keywords']
+                    except KeyError:
+                        pictograms[data['_id']] = {
+                            '_id': data['_id'],
+                            'sex': data['sex'],
+                            'violence': data['violence'],
+                            'keywords': {
+                                lang: data['keywords']
+                            }
+                        }
+    db[COLLECTION_NAME].insert_many(list(pictograms.values()))
     print('Data inserted successfully')
 
-db[COLLECTION_NAME].create_index([('keywords.keyword', pymongo.ASCENDING)], name='keywords.keyword_1')
-db[COLLECTION_NAME].create_index([('keywords.plural', pymongo.ASCENDING)], name='keywords.plural_1')
+for lang in langs:
+    lang_key = f'keywords.{lang}'
+    db[COLLECTION_NAME].create_index([(f'{lang_key}.keyword', pymongo.ASCENDING)], name=f'{lang_key}.keyword_1')
+    db[COLLECTION_NAME].create_index([(f'{lang_key}.plural', pymongo.ASCENDING)], name=f'{lang_key}.plural_1')
 print('Indexes created successfully')
