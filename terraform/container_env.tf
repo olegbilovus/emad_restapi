2414e5d10dc3368021ba46eb3265d0ae32a682d0
Oleg Bilovus <68842946+olegbilovus@users.noreply.github.com>
2024-10-31 14:01:58 +0100

    added dalle3 envs to restapi

--- terraform/container_env.tf
+++ terraform/container_env.tf
@@ -217,6 +217,14 @@ resource "azurerm_container_app" "this" {
         name  = "CORE_URL_EN"
         value = "https://${azurerm_container_app.core["en"].ingress[0].fqdn}"
       }
+      env {
+        name  = "DALLE3_ENDPOINT"
+        value = "${azurerm_cognitive_account.openai.endpoint}openai/deployments/${azurerm_cognitive_deployment.dalle3.name}/images/generations?api-version=2024-06-01"
+      }
+      env {
+        name  = "DALLE3_APIKEY"
+        value = azurerm_cognitive_account.openai.primary_access_key
+      }
     }
     min_replicas = var.scale.min
     max_replicas = var.scale.max
